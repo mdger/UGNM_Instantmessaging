8b245b1d688014e147c737226ee08ae5fe184365
Tom Acker <thomas.acker@rwth-aachen.de>
2014-11-06 01:01:18 +0100

    Rumprobieren mit den Service Files

--- LAS2peer-Template-Project/doc/curl/script.sh
+++ LAS2peer-Template-Project/doc/curl/script.sh
@@ -3,8 +3,9 @@ URL='http://localhost:8080/example'
 echo "curl test script for las2peer service"
 
 echo "test authentication with test user alice"
-curl -v -X GET $URL/validate --user alice:pwalice
+#curl -v -X GET $URL/validate --user alice:pwalice
 #curl -v -X GET $URL/message/group/TestGroup
+curl -v -X localhost:8080/example
 echo 
 echo "PRESS RETURN TO CONTINUE..."
 read
